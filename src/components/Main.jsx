daec93fab3c07a49df2115a98213ffeb3737f870
antran1245 <antran1245@gmail.com>
2022-07-09 23:32:05 -0700

    remove useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useRef } from "react";
 import Converter from "./Converter";
 import Heading from "./Heading";
@@ -6,7 +5,6 @@ import Info from "./Info";
 
 export default function Main() {
     const convertRef = useRef(null);
-    const [position, setPosition] = useState()
     const scroll = () => {
         convertRef.current.scrollIntoView({behavior:"smooth"})
     }
