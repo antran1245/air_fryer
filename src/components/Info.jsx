d981cd262f4b34ec4fed27794040d29dba6c33cf
antran1245 <antran1245@gmail.com>
2022-07-09 21:11:25 -0700

    css snap section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,12 +1,16 @@
+import '../sass/info.scss';
+
 export default function Info() {
     return(
-        <section>
-            <h3>Information</h3>
-            <h4>Conversion</h4>
-            <ul>
-                <li>Time: Reduce by 20%</li>
-                <li>Temperature: Decrease by 25</li>
-            </ul>
+        <section id="info">
+            <div>
+                <h2>Information</h2>
+                <h4><u>Conversion</u></h4>
+                <ul>
+                    <li>Time: Reduce by 20%</li>
+                    <li>Temperature: Decrease by 25 &#176;F</li>
+                </ul>
+            </div>
         </section>
     );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,20 @@
-import { useEffect, useRef } from "react";
+import { useState } from "react";
+import { useRef } from "react";
 import Converter from "./Converter";
 import Heading from "./Heading";
+import Info from "./Info";
 
 export default function Main() {
     const convertRef = useRef(null);
+    const [position, setPosition] = useState()
     const scroll = () => {
         convertRef.current.scrollIntoView({behavior:"smooth"})
     }
-    
     return(
         <>
             <Heading scroll={scroll}/>
             <Converter convertRef={convertRef}/>
+            <Info/>
         </>
     );
 }
